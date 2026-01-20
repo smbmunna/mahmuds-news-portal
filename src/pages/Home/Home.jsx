4a2da3a80d933dbbece711f789e6e5e5f44f4ea4
smbmunna <smbmunna@gmail.com>
2026-01-20 11:23:58 +0600

    Country dropdown added with country data

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 
 
-const Home=()=>{
+const Home = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('countries.json')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+
+    const handleSelectCountry=(code)=>{
+        fetch(`https://newsdata.io/api/1/latest?apikey=pub_8d3348ec0319466997ce59e93a60e686&country=${code}`)        
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    }
     return (
         <div>
-            <h1>Home page</h1>
+            <h1>Home page : {countries.length}</h1>
+            <div className="dropdown dropdown-center">
+                <div tabIndex={0} role="button" className="btn m-1">Click to select a Country  ⬇️</div>
+                <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    {
+                        countries.map(country => <li key={country.code}>
+                            <a onClick={()=>handleSelectCountry(country.code)}>{country.name}</a>
+                        </li>)
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
